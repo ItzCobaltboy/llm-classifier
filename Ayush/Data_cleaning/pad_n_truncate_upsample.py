3fb772cd6b63764b3bffbb2d55c7c441ac709e4d
yadavayushyadav <50135360+yadavayushyadav@users.noreply.github.com>
2026-04-04 00:01:47 +0530

    Update pad_n_truncate_upsample.py

--- Ayush/Data_cleaning/pad_n_truncate_upsample.py
+++ Ayush/Data_cleaning/pad_n_truncate_upsample.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import gc
+gc.collect()
 from utils import *
 
 for size in ["small", "medium", "original"]:
     for split in ["train", "val", "test"]:
         print(f"Processing pad_n_truncate_upsample: {size} | {split}")
         df = pd.read_parquet(f'./Dataset/Dataset_{size}/{split}/{split}.parquet')
+
+        #df = clean_noise(df)            # Remove URLs, HTML tags and extra whitespaces
+        #df = lowercase_text(df)         # Convert to lowercase
+        #df = remove_punctuation(df)     # Remove Punctuation marks
         
         df_final = upsample(remove_duplicates(pad_n_truncate(df)))
         
