249092b4b0f9e288884cf21fb9eb4787a8c3a8dd
yadavayushyadav <50135360+yadavayushyadav@users.noreply.github.com>
2026-04-04 00:02:24 +0530

    Add gc.collect() before dataset processing
    Added garbage collection call before processing datasets.

--- Ayush/Data_cleaning/truncate_downsample.py
+++ Ayush/Data_cleaning/truncate_downsample.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import gc
+gc.collect()
 from utils import *
 
 for size in ["small", "medium", "original"]:
     for split in ["train", "val", "test"]:
         print(f"Processing truncate_downsample: {size} | {split}")
         df = pd.read_parquet(f'./Dataset/Dataset_{size}/{split}/{split}.parquet')
+
+        #df = clean_noise(df)            # Remove URLs, HTML tags and extra whitespaces
+        #df = lowercase_text(df)         # Convert to lowercase
+        #df = remove_punctuation(df)     # Remove Punctuation marks
         
         df_final = downsample(remove_duplicates(truncate_only(df)))
         
